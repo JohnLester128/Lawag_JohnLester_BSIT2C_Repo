eb12f2dc2786d288d4b66f944712d0b4cccc72c8
Johnlester128 <jlawag@filamer.edu.ph>
2025-04-21 09:29:54 +0800

    battleship v3

--- battleship/battleship_v2.js
+++ battleship/battleship_v2.js
@@ -1,51 +1,51 @@
-
-var randomLoc = Math.floor(Math.random() * 5);
+var randomLoc = Math.floor(Math.random() *5);
 
 var location1 = randomLoc;
 var location2 = location1 + 1;
 var location3 = location2 + 1;
 
+var guess;
+var hits = 0;
+var guesses = 0;
 
-let guess;
-let hits = 0;
-let guesses = 0;
+var isSunk = false;
 
-let isSunk = false;
+// Game logic
 
-// game logic
 while (isSunk == false){
+    guess = prompt("Ready!, Aim, Fire! (Enter a number 0-6): ");
 
-    if(guesses == null){
-        alert("boom");
+    if (guess == null) {
+        alert("Thank you for playing!");
         break;
     }
 
-    guess = prompt("Ready!. Aim!. Fire (Enter a number 0-6): ")
-    if (guess < 0 || guess > 6){
-        alert("Please enter a valid cell number")
-    }
-    else{
-        guess +=1;
-        if (guess == location1 || guess == location2 || guess == location3 ){
-            hits +=1;
-            alert("Hit!")
-            if (hits == 3){
-                isSunk = True;
-                alert("You sunk my battleship!")
-            }  
+    if (guess < 0 || guess > 6) {
+        alert("Please enter a valid cell number!");
+    }else {
+        guesses = guesses + 1;
+        if(guess == location1 || guess == location2 || guess == location3){
+            hits = hits + 1;
+            alert ("hits")
+            if (hits == 3) {
+                inSunk = true;
+                alert("You sank my battleship!")
+            }
         }
         else{
             alert("Miss!")
+            if (guesses >= 10) {
+                alert("You exceeded the maximum attempts!");
+                break;
+            }
         }
     }
 }
 
-var accuracy = (guess > 0 ? ( hits/ guesses) * 100 : 0).toFixed(2);
+var accuracy = (guess > 0 ? (hits / guess) * 100 : 0). toFixed(3);
 
-var stats = "You took " + guesses + "guesses to sink the battleship  " + "\n" + "Accuracy:" + accuracy + "%";
+var stats = "You took " +guesses + " guesses to sink the battleship! " + "\n" + "Accuracy: " + accuracy + "%";
 
+var stats = "You sank my battleship at: " + guesses + "guesses" + "and your accuracy is: " + (3/guesses);
 
-
-
-
-
+alert(stats);
